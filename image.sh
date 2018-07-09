f21f2dfd04c5f9b958d1a295e33902bc93595d15
Torrance <torrance123@gmail.com>
2018-07-09 11:06:02 +0800

    Add weighting as parameter to image.sh; also set cores for wsclean to 20

--- image.sh
+++ image.sh
@@ -13,6 +13,12 @@ set -e
 set -x
 
 obsid=$1
+weight=$2
+
+if [[ -z $weight ]]; then
+  echo "No CLEAN weighting specified"
+  exit 1
+fi
 
 touch image_started
 
@@ -27,8 +33,9 @@ if [[ ! -f chgcentred ]]; then
   touch chgcentred
 fi
 
-wsclean $absmem -name wsclean-final -multiscale -mgain 0.85 -pol xx,xy,yx,yy -joinpolarizations -weight briggs 0 -size 8000 8000 -scale 0.0034 -niter 1000000 -auto-threshold 1 -auto-mask 3 ${obsid}.ms
+name=$(echo $weight | tr -d '[:spaces:]')
+wsclean $absmem -j 20 -name wsclean-final-${name} -multiscale -mgain 0.85 -pol xx,xy,yx,yy -joinpolarizations -weight $weight -size 8000 8000 -scale 0.0034 -niter 1000000 -auto-threshold 1 -auto-mask 3 ${obsid}.ms
 
-pbcorrect wsclean-final image.fits beam stokes-final
+pbcorrect wsclean-final-${name} image.fits beam stokes-final-${name}
 
 rm image_started && touch image_complete

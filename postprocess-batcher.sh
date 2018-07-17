d9af2f29afdc9206e3cdc9e816ae3167b622fd94
Torrance <torrance123@gmail.com>
2018-07-17 09:42:03 +0800

    Create images sequentially — wsclean will fail with tmp data writing errors otherwise

--- postprocess-batcher.sh
+++ postprocess-batcher.sh
@@ -17,6 +17,6 @@ grep -v '^#' $1 | while read obsid; do
   jobid=$(sbatch -J "cal.sh $obsid" --workdir $obsid cal.sh $obsid | cut -d ' ' -f 4)
   jobid=$(sbatch -J "selfcal.sh $obsid 1" --workdir $obsid -d afterok:$jobid selfcal.sh $obsid 1 | cut -d ' ' -f 4)
   jobid=$(sbatch -J "selfcal.sh $obsid 2" --workdir $obsid -d afterok:$jobid selfcal.sh $obsid 2 | cut -d ' ' -f 4)
-  sbatch -J "image.sh $obsid briggs 0" --workdir $obsid -d afterok:$jobid image.sh $obsid 'briggs 0'
+  jobid=$(sbatch -J "image.sh $obsid briggs 0" --workdir $obsid -d afterok:$jobid image.sh $obsid 'briggs 0' | cut -d ' ' -f 4)
   sbatch -J "image.sh $obsid natural" --workdir $obsid -d afterok:$jobid image.sh $obsid 'natural'
 done

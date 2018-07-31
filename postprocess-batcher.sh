7da919885e912aa134e4cc82020105bdb8cbc318
Torrance <torrance123@gmail.com>
2018-07-31 11:48:27 +0800

    Perform only one selfcal loop (the second reduced both the rms and total flux in the same ratio - ie. no benefit)

--- postprocess-batcher.sh
+++ postprocess-batcher.sh
@@ -22,22 +22,14 @@ grep -v '^#' $1 | while read obsid; do
   fi
 
   jobid=$(cat $obsid/cal_post_scheduled 2>/dev/null || cat $obsid/cal_post_started 2>/dev/null || cat $obsid/cal_post_complete 2>/dev/null || echo '')
-  if [[ -n $jobid && ! -f $obsid/selfcal1_scheduled && ! -f $obsid/selfcal1_started && ! -f $obsid/selfcal1_complete ]]; then
-    jobid=$(sbatch -J "selfcal.sh $obsid 1" --workdir $obsid -d afterok:$jobid selfcal.sh $obsid selfcal1 post | cut -d ' ' -f 4)
+  if [[ -n $jobid && ! -f $obsid/selfcal_scheduled && ! -f $obsid/selfcal_started && ! -f $obsid/selfcal_complete ]]; then
+    jobid=$(sbatch -J "selfcal.sh $obsid 1" --workdir $obsid -d afterok:$jobid selfcal.sh $obsid selfcal post | cut -d ' ' -f 4)
     if [[ -n $jobid ]]; then
-      echo $jobid > $obsid/selfcal1_scheduled
+      echo $jobid > $obsid/selfcal_scheduled
     fi
   fi
 
-  jobid=$(cat $obsid/selfcal1_scheduled 2>/dev/null || cat $obsid/selfcal1_started 2>/dev/null || cat $obsid/selfcal1_complete 2>/dev/null || echo '')
-  if [[ -n $jobid && ! -f $obsid/selfcal2_scheduled && ! -f $obsid/selfcal2_started && ! -f $obsid/selfcal2_complete ]]; then
-    jobid=$(sbatch -J "selfcal.sh $obsid 2" --workdir $obsid -d afterok:$jobid selfcal.sh $obsid selfcal2 selfcal1 | cut -d ' ' -f 4)
-    if [[ -n $jobid ]]; then
-      echo $jobid > $obsid/selfcal2_scheduled
-    fi
-  fi
-
-  jobid=$(cat $obsid/selfcal2_scheduled 2>/dev/null || cat $obsid/selfcal2_started 2>/dev/null || cat $obsid/selfcal2_complete 2>/dev/null || echo '')
+  jobid=$(cat $obsid/selfcal_scheduled 2>/dev/null || cat $obsid/selfcal_started 2>/dev/null || cat $obsid/selfcal_complete 2>/dev/null || echo '')
   if [[ -n $jobid && ! -f $obsid/image_uniform_scheduled && ! -f $obsid/image_uniform_started && ! -f $obsid/image_uniform_complete ]]; then
     jobid=$(sbatch -J "image.sh $obsid uniform" --workdir $obsid -d afterok:$jobid image.sh $obsid uniform | cut -d ' ' -f 4)
     if [[ -n $jobid ]]; then

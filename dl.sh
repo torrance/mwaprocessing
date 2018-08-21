faa4930f784c7315cb68b3385ef49695a2616f84
Torrance <torrance123@gmail.com>
2018-08-21 11:12:56 +0800

    Update dl.sh to use mwa_client and AVSO

--- dl.sh
+++ dl.sh
@@ -1,18 +1,29 @@
 #! /bin/bash
-#SBATCH -M zeus
-#SBATCH --partition copyq
-#SBATCH --nodes=1
-#SBATCH --ntasks-per-node=1
-#SBATCH --mail-type FAIL,TIME_LIMIT,TIME_LIMIT_90
-#SBATCH --mail-user torrance.hodgson@postgrad.curtin.edu.au
-#SBATCH --export=NONE
-#SBATCH --time=02:00:00
 
 set -e
 set -x
-if [[ ! -d $1 ]]; then
-  mkdir $1
+
+if [[ -z $1 ]]; then
+  echo "No obsid list supplied"
+  exit 1
 fi
-touch $1/download_started
-obsdownload.py -o $1
-rm $1/download_started && touch $1/download_complete
+
+rm obsid.csv || true
+touch obsid.csv
+
+for obsid in $(cat $1); do
+  echo "obs_id=${obsid}, job_type=c, timeres=4, freqres=40, edgewidth=80, conversion=ms, allowmissing=true, flagdcchannels=true" >> obsid.csv
+done
+
+mwa_client -c obsid.csv -d .
+
+for obsid in $(cat $1); do
+  if [[ -f ${obsid}_ms.zip ]]; then
+    if [[ ! -d ${obsid} ]]; then
+      mkdir $obsid
+    fi
+    unzip -u -d ${obsid} ${obsid}_ms.zip
+  else
+    echo "No zip file found for $obsid"
+  fi
+done

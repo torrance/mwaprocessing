c088335cd2b769ad18444f1ec5ebda37a584c43d
Torrance Hodgson <torrance123@gmail.com>
2018-09-25 16:41:48 +0800

    Add pointing.py

---
+++ pointing.py
@@ -0,0 +1,22 @@
+#! /usr/bin/env python
+from __future__ import print_function, division
+
+import argparse
+
+from astropy.coordinates import SkyCoord
+from astropy.io.fits import getheader
+import astropy.units as u
+
+
+def main():
+    parser = argparse.ArgumentParser()
+    parser.add_argument('--meta', required=True)
+    args = parser.parse_args()
+
+    header = getheader(args.meta)
+    pointing = SkyCoord(header['RA'], header['DEC'], unit=(u.degree, u.degree))
+    print(pointing.to_string('hmsdms'))
+
+
+if __name__ == '__main__':
+    main()

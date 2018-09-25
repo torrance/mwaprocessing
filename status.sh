6d41cb0aa94527957f801c15959e12a885084913
Torrance <torrance123@gmail.com>
2018-09-25 16:49:33 +0800

    Filter out new Galaxy output for job status

--- status.sh
+++ status.sh
@@ -2,5 +2,5 @@
 
 find . -name '*_scheduled' -or -name '*_started' -or -name '*_complete' | grep -v download | while read line; do
   jobid=$(cat $line);
-  sacct --noheader -j ${jobid} --format=JobID,Jobname%30,State,Start,End,Elapsed,Time | grep -v '.bat+' | tail -n 1;
+  sacct --noheader -j ${jobid} --format=JobID,Jobname%30,State,Start,End,Elapsed,Time | grep -v '\.bat+' | grep -v '\.ext' | tail -n 1;
 done

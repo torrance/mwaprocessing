8e65852bb9ee3f930d2d9e739d93b9b0890286b0
Torrance Hodgson <torrance123@gmail.com>
2019-02-20 17:04:39 +0800

    Add option to output degrees for pointing

--- pointing.py
+++ pointing.py
@@ -11,11 +11,15 @@ import astropy.units as u
 def main():
     parser = argparse.ArgumentParser()
     parser.add_argument('meta')
+    parser.add_argument('--degrees', action='store_true')
     args = parser.parse_args()
 
     header = getheader(args.meta)
     pointing = SkyCoord(header['RA'], header['DEC'], unit=(u.degree, u.degree))
-    print(pointing.to_string('hmsdms'))
+    if args.degrees:
+        print(pointing.ra.deg, pointing.dec.deg)
+    else:
+        print(pointing.to_string('hmsdms'))
 
 
 if __name__ == '__main__':

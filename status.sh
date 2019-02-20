ffbfd912846e5d7113d8a5554c41daf78fbb1347
Torrance <torrance123@gmail.com>
2019-02-20 17:06:04 +0800

    Allow for partial status matches

--- status.sh
+++ status.sh
@@ -1,6 +1,8 @@
 #! /bin/bash
+name="$1"
+echo Searching with $name
 
-find . -name '*_scheduled' -or -name '*_started' -or -name '*_complete' | grep -v download | while read line; do
+find . -name "*${name}_scheduled" -or -name "*${name}_started" -or -name "*${name}_complete" | grep -v download | while read line; do
   jobid=$(cat $line);
   sacct --noheader -j ${jobid} --format=JobID,Jobname%30,State,Start,End,Elapsed,Time | grep -v '\.bat+' | grep -v '\.ext' | tail -n 1;
 done

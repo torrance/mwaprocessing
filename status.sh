7c0d5380aa90345372e4ee0f70bde65becd64355
Torrance <torrance123@gmail.com>
2019-05-23 13:43:04 +0800

    Allow status to apply to jobs run in the past (SLRUM update caused a change in behaviour)

--- status.sh
+++ status.sh
@@ -4,5 +4,5 @@ echo Searching with $name
 
 find . -name "*${name}_scheduled" -or -name "*${name}_started" -or -name "*${name}_complete" | grep -v download | while read line; do
   jobid=$(cat $line);
-  sacct --noheader -j ${jobid} --format=JobID,Jobname%30,State,Start,End,Elapsed,Time | grep -v '\.bat+' | grep -v '\.ext' | tail -n 1;
+  sacct -S 2018-01-01 --noheader -j ${jobid} --format=JobID,Jobname%30,State,Start,End,Elapsed,Time | grep -v '\.bat+' | grep -v '\.ext' | tail -n 1;
 done

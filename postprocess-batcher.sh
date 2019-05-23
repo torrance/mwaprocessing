658d37d47bb0061768fa799d458e4b3e49d78b6b
Torrance <torrance123@gmail.com>
2019-05-23 13:43:59 +0800

    Remove second model calibration

--- postprocess-batcher.sh
+++ postprocess-batcher.sh
@@ -14,16 +14,8 @@ grep -v '^#' $1 | while read obsid; do
   fi
 
   jobid=$(cat $obsid/cal_pre_scheduled 2>/dev/null || cat $obsid/cal_pre_started 2>/dev/null || cat $obsid/cal_pre_complete 2>/dev/null || echo '')
-  if [[ -n $jobid && ! -f $obsid/cal_post_scheduled && ! -f $obsid/cal_post_started && ! -f $obsid/cal_post_complete ]]; then
-    jobid=$(sbatch -J "cal.sh $obsid post" --workdir $obsid -d afterok:$jobid cal.sh $obsid post pre | cut -d ' ' -f 4)
-    if [[ -n $jobid ]]; then
-      echo $jobid > $obsid/cal_post_scheduled
-    fi
-  fi
-
-  jobid=$(cat $obsid/cal_post_scheduled 2>/dev/null || cat $obsid/cal_post_started 2>/dev/null || cat $obsid/cal_post_complete 2>/dev/null || echo '')
   if [[ -n $jobid && ! -f $obsid/selfcal_scheduled && ! -f $obsid/selfcal_started && ! -f $obsid/selfcal_complete ]]; then
-    jobid=$(sbatch -J "selfcal.sh $obsid 1" --workdir $obsid -d afterok:$jobid selfcal.sh $obsid selfcal post | cut -d ' ' -f 4)
+    jobid=$(sbatch -J "selfcal.sh $obsid 1" --workdir $obsid -d afterok:$jobid selfcal.sh $obsid selfcal pre | cut -d ' ' -f 4)
     if [[ -n $jobid ]]; then
       echo $jobid > $obsid/selfcal_scheduled
     fi
